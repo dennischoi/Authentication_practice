9a4a47e67835cdffee9a6687df58a33c207cedb0
Dennis Choi <dennis.choi7@gmail.com>
2016-04-12 11:21:16 -0400

    Implement User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  #
+  # get 'users/create'
+
+  root "users#new"
+
+  resource :users, only: %i(new create)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
